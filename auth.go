3cafd0dc459a1fccdc8fd7ba099000671e0be611
Placebo27 <placebo27@hust.edu.cn>
2023-04-19 12:31:38 +0800

    fix: catch file close error
    catch the return value of file Close function or log an exception when it is not nil
    
    Log: catch file close error

--- auth.go
+++ auth.go
@@ -55,6 +55,8 @@ func readAuthority(hostname, display string) (
 	defer func() {
 		if closeErr := r.Close(); err == nil {
 			err = closeErr
+		}else {
+			log.Printf("error on close file %v: %v", r.Name(), closeErr)
 		}
 	}()
 
